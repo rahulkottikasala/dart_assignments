b3c9420b24d75e231c60f093d0c37f00a3d96ab2
rahulkottikasala <rahulkottikasala@gmail.com>
2022-10-12 20:28:46 +0530

    add some functions

--- bin/dart_assignments.dart
+++ bin/dart_assignments.dart
@@ -3,22 +3,22 @@ void main() {
     {
       "name": "rahul",
       "age": 10,
-      "marks": [21, 35, 5],
+      "marks": [21, 35, 15],
     },
     {
       "name": "amal",
       "age": 11,
-      "marks": [21, 35, 5],
+      "marks": [8, 13, 21],
     },
     {
       "name": "ajith",
       "age": 13,
-      "marks": [432, 35, 5],
+      "marks": [12, 26, 5],
     },
     {
       "name": "john",
       "age": 12,
-      "marks": [21, 23, 432],
+      "marks": [21, 23, 43],
     }
   ];
   stuudentDetails(students);
@@ -26,12 +26,15 @@ void main() {
 
 void stuudentDetails(List<Map<String, dynamic>> students) {
     print("-----------------------------");
-  
   for (int i = 0; i < students.length; i++) {
+    num totalMark = findTotalMark(students[i]["marks"]);
+    String status =  passOrFail(totalMark);
     print("Student details \n");
     print("name :  ${students[i]["name"]}");
     print("age : ${students[i]["age"].toString()}");
-    print("Total mark :  ${findTotalMark(students[i]["marks"]).toString()}");
+    print("Total mark :  ${totalMark.toString()}");
+    print("Status :  $status");
+   
     print("-----------------------------");
   }
 }
@@ -40,3 +43,12 @@ num findTotalMark(List<num> marks) {
   num totalMark = marks[0] + marks[1] + marks[2];
   return totalMark;
 }
+ String passOrFail(num mark){
+   String status = "failed";
+   if(mark>=50){
+     status = "passed";
+   }
+   return status;
+}
+
+
