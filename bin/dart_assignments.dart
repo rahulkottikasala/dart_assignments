fbff50d1e7e4042328645271956b54ccc1a2a41c
rahulkottikasala <rahulkottikasala@gmail.com>
2022-10-12 19:51:19 +0530

    named parameters

--- bin/dart_assignments.dart
+++ bin/dart_assignments.dart
@@ -1,5 +1,57 @@
-import 'package:dart_assignments/dart_assignments.dart' as dart_assignments;
+// void main()
+// {
+// sum();
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_assignments.calculate()}!');
+// sumParams(2,4);
+
+// var sum = sumReturn(100,200);
+// print(sum);
+// print(sumReturn(100,200));
+
+// sumReq(num1:10, num2:20, num3:30);
+//  sumFunction(10, 29,(int num1, int num2){
+//   print(num1*num2);
+//  });
+
+// }
+
+// void sum()
+// {
+// print(2+3);
+// }
+
+// void sumParams(int num1, int num2)
+// {
+// print(num1+num2);
+// }
+
+// int sumReturn(int num1, int num2)
+// {
+// return(num1+num2);
+// }
+
+//Named Parameters
+
+// void sumReq({required int num1, required int num2, int num3 = 0})
+// {
+// print(num1+num2+num3);
+// }
+
+// sumFunction(int a, int b, void Function(int, int) customFunction){
+//     customFunction(a,b);
+//   }
+
+void main()async {
+  await sum();
+  print('after Future');
+}
+
+Future<int> sumFuture(int a, int b) async {
+  await Future.delayed(Duration(seconds: 3));
+  print('In sum future ${a + b}');
+  return a + b;
+}
+ Future <void> sum() async {
+  await sumFuture(20, 20);
+  print('in just sum');
 }
