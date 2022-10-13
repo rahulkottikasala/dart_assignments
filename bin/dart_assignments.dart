68c2db51b3648d2415e3b8ee00c441b9f21d0adf
rahulkottikasala <rahulkottikasala@gmail.com>
2022-10-13 12:15:23 +0530

    reuse functions

--- bin/dart_assignments.dart
+++ bin/dart_assignments.dart
@@ -1,5 +1,45 @@
-import 'package:dart_assignments/dart_assignments.dart' as dart_assignments;
+ void main() {
+  Students arun = Students("arun",15,[23,34,18]);
+  Students rahul = Students("rahul",15,[23,34,18]);
+  Students ajith = Students("ajith",15,[23,34,18]);
+  Students sonu = Students("sonu",15,[23,34,18]);
+arun.studentDetails();
+rahul.studentDetails();
+ajith.studentDetails();
+sonu.studentDetails();
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_assignments.calculate()}!');
+
+class Students{
+String name;
+num age;
+List <num> marks;
+Students(this.name, this.age, this.marks);
+
+
+
+ studentDetails() {
+    print("-----------------------------");
+    num totalMark = findTotalMark(marks);
+    String status =  passOrFail(totalMark);
+    print("Student details \n");
+    print("name : $name");
+    print("age : $age");
+    print("Total mark :  $totalMark");
+    print("Status :  $status");
+   
+    print("-----------------------------");
+}
+}
+
+num findTotalMark(List<num> marks) {
+  num totalMark = marks[0] + marks[1] + marks[2];
+  return totalMark;
+}
+ String passOrFail(num mark){
+   String status = "failed";
+   if(mark>=50){
+     status = "passed";
+   }
+   return status;
 }
